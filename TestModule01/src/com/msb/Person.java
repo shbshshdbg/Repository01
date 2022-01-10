cca6e7e594ffd6465f1789ba72d2bc7df16f0689
“shbshshdbg” <2603464178@qq.com>
2022-01-10 16:39:14 +0800

    在Person类中添加了age属性

---
+++ TestModule01/src/com/msb/Person.java
@@ -0,0 +1,11 @@
+package com.msb;
+
+/**
+ * @Auther: shbshshdbg
+ * @Date: 2022/1/10 - 01 - 10 - 16:38
+ * @Description: com.msb
+ * @version: 1.0
+ */
+public class Person {
+    private int age;
+}
