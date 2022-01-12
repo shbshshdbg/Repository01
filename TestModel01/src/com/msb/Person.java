29f88141bb3812097bfb3f3e9497b4f5dd561301
Charris <shbshshdbg@foxmail.com>
2022-01-12 23:05:04 +0800

    在Person类中添加了age属性

---
+++ TestModel01/src/com/msb/Person.java
@@ -0,0 +1,11 @@
+package com.msb;
+
+/**
+ * @Auther: shbshshdbg
+ * @Date: 2022/1/12 - 01 - 12 - 23:04
+ * @Description: com.msb
+ * @version: 1.0
+ */
+public class Person {
+    private int age;
+}
